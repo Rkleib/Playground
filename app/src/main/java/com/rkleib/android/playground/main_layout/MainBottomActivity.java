7dbaa28741a791f6c0b68df9b59a5f85748c37c1
huzairuje <metalh8@gmail.com>
2020-08-10 23:59:15 +0700

    fix identation

--- app/src/main/java/com/rkleib/android/playground/main_layout/MainBottomActivity.java
+++ app/src/main/java/com/rkleib/android/playground/main_layout/MainBottomActivity.java
@@ -16,16 +16,23 @@ public class MainBottomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_bottom_navigation);
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        BottomNavigationView bottomNavigationView =
+                (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.
+                OnNavigationItemSelectedListener() {
+
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.page_1:
-                        Toast.makeText(MainBottomActivity.this, "Menu 1", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainBottomActivity.this, "Menu 1",
+                                Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.page_2:
-                        Toast.makeText(MainBottomActivity.this, "Menu 2", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainBottomActivity.this, "Menu 2",
+                                Toast.LENGTH_SHORT).show();
                         break;
                     default:
                         return true;
